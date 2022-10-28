3b9755cdc70a5c95c2582b4a94ec1fe8437b2762
Arturo Garza-Rodriguez <arturogr-dev@gmail.com>
2022-10-27 17:32:07 -0700

    Add command line flags in sorting benchmark.

--- examples/sorting/test/sorting_benchmark.cc
+++ examples/sorting/test/sorting_benchmark.cc
@@ -12,22 +12,59 @@
 #include <vector>
 
 #include "examples/sorting/include/sorting.h"
+#include "examples/sorting/test/sorting_init.h"
 
-// Experiment sorting data sizes that could potentially fit in L2, L3 and DRAM.
-static constexpr int kDataSizeBegin = 15;  //   128 kB (Assuming 4 bytes ints).
-static constexpr int kDataSizeEnd = 23;    // 32768 kB (Assuming 4 bytes ints).
-static constexpr int kDataSizeStep = 1;    // Input size incremental step.
+namespace sorting_init {
+
+// Define the declared command line flags in the `sorting_init` namespace.
+//
+//   + `data_shift` determines the size of the input vector to be sorted. This
+//     represents the number of shifts to be performed to generate the input
+//     data size. Ensuring an input size of power of 2 for bitonicsort.
+//
+//   + `segment_size` determines the number of elements in each segment. This
+//     would also represent the size of each individual smaller sort.
+//
+//   + `num_threads` determines the number of execution threads to be launched
+//     in the different parallel implementations.
+//
+// For example, if you run:
+//
+//   $ make benchmark benchmark_args=--data_shift=23\ --segment_size=2048
+//
+// It will test the following scenario, assuming 32-bit integers:
+//
+//   -> data_size = 1 << 22 = 4194304 [elements] = 16384 [kB]
+//   -> segment_size = 2048 [elements] =  8192 [bytes]
+//
+// Another example of usage is:
+//
+//   $ make benchmark benchmark_args=--data_shift=15\ --segment_size=1024
+
+MODCNCY_DEFINE_int32(data_shift, 22);
+MODCNCY_DEFINE_int32(segment_size, 2048);
+MODCNCY_DEFINE_int32(num_threads, std::thread::hardware_concurrency());
+
+}  // namespace sorting_init
 
 // =============================================================================
-// Helper to compute the logarithm base 2 of a power of 2.
+// Computes the logarithm base 2 of a power of 2.
 static inline int log2(int x) { return __builtin_ctz(x); }
 
 // =============================================================================
-// Helper to verify if data is sorted.
-static bool IsSorted(const std::vector<int>& data) {
+// Verifies if the data is sorted.
+static bool is_sorted(const std::vector<int>& data) {
   for (size_t i = 1; i < data.size(); ++i)
     if (data[i - 1] > data[i]) return false;
   return true;
+}
+
+// =============================================================================
+// Verifies if a sequential implementation is executed.
+static bool is_sequential(sorting::Type type) {
+  return type == sorting::Type::kStdSort ||
+         type == sorting::Type::kOriginalBitonicsort ||
+         type == sorting::Type::kSegmentedBitonicsort;
 }
 
 // =============================================================================
@@ -35,13 +72,10 @@ static bool IsSorted(const std::vector<int>& data) {
 template <sorting::Type type>
 static void BM_Sort32BitInts(benchmark::State& state) {  // NOLINT
   // Setup.
-  const size_t data_size = 1 << state.range(0);  // Ensures a power of 2.
-  const size_t segment_size = 2048;              // Elements.
-  size_t num_threads = std::thread::hardware_concurrency();
-  if (type == sorting::Type::kStdSort ||
-      type == sorting::Type::kOriginalBitonicsort ||
-      type == sorting::Type::kSegmentedBitonicsort)
-    num_threads = 1;
+  using namespace sorting_init;  // NOLINT(build/namespaces)
+  const size_t data_size = 1 << FLAGS_data_shift;
+  const size_t segment_size = FLAGS_segment_size;
+  const size_t num_threads = is_sequential(type) ? 1 : FLAGS_num_threads;
   std::vector<int32_t> data;
   data.reserve(data_size);
   for (size_t i = 0; i < data_size; ++i)
@@ -49,7 +83,7 @@ static void BM_Sort32BitInts(benchmark::State& state) {  // NOLINT
   std::random_device rand_dev;
   std::mt19937 gen(rand_dev());
   std::shuffle(data.begin(), data.end(), gen);
-  assert(!IsSorted(data) && "Data should not be sorted");
+  assert(!is_sorted(data) && "Data should not be sorted after shuffle");
 
   // Benchmark.
   for (auto _ : state) {
@@ -57,9 +91,9 @@ static void BM_Sort32BitInts(benchmark::State& state) {  // NOLINT
 
     // Prepare for next iteration.
     state.PauseTiming();
-    assert(IsSorted(data) && "Data should be sorted");
+    assert(is_sorted(data) && "Data should be sorted");
     std::shuffle(data.begin(), data.end(), gen);
-    assert(!IsSorted(data) && "Data should not be sorted");
+    assert(!is_sorted(data) && "Data should not be sorted after shuffle");
     state.ResumeTiming();
   }
 
@@ -68,40 +102,43 @@ static void BM_Sort32BitInts(benchmark::State& state) {  // NOLINT
   const int segment_in_bytes = segment_size * sizeof(int);
   const int num_segments = data_size / segment_size;
   const int log_num_segments = log2(num_segments);
-  const int bitonic_stages = (log_num_segments * (log_num_segments + 1)) / 2;
+  const int barrier_stages = (log_num_segments * (log_num_segments + 1)) / 2;
   const std::string label =
-      std::to_string(data_in_bytes / 1024) + " kB data | " +
-      std::to_string(segment_in_bytes / 1024) + " kB segment | " +
+      std::to_string(data_in_bytes / 1024) + " [kB] data | " +
+      std::to_string(segment_in_bytes) + " [bytes] segment | " +
       std::to_string(num_segments) + " segments | " +
-      std::to_string(bitonic_stages) + " bitonic stages | " +
+      std::to_string(barrier_stages) + " barrier stages | " +
       std::to_string(num_threads) + " threads ";
   state.SetLabel(label);
   state.SetBytesProcessed(state.iterations() * data_in_bytes);
 }
 
+// Register benchmarks.
 BENCHMARK_TEMPLATE(BM_Sort32BitInts, sorting::Type::kStdSort)
-    ->DenseRange(kDataSizeBegin, kDataSizeEnd, kDataSizeStep)
     ->Unit(benchmark::kMillisecond)
     ->UseRealTime();
 BENCHMARK_TEMPLATE(BM_Sort32BitInts, sorting::Type::kOriginalBitonicsort)
-    ->DenseRange(kDataSizeBegin, kDataSizeEnd, kDataSizeStep)
     ->Unit(benchmark::kMillisecond)
     ->UseRealTime();
 BENCHMARK_TEMPLATE(BM_Sort32BitInts, sorting::Type::kSegmentedBitonicsort)
-    ->DenseRange(kDataSizeBegin, kDataSizeEnd, kDataSizeStep)
     ->Unit(benchmark::kMillisecond)
     ->UseRealTime();
 BENCHMARK_TEMPLATE(BM_Sort32BitInts, sorting::Type::kOmpBasedBitonicsort)
-    ->DenseRange(kDataSizeBegin, kDataSizeEnd, kDataSizeStep)
     ->Unit(benchmark::kMillisecond)
     ->UseRealTime();
 BENCHMARK_TEMPLATE(BM_Sort32BitInts, sorting::Type::kNonBlockingBitonicsort)
-    ->DenseRange(kDataSizeBegin, kDataSizeEnd, kDataSizeStep)
     ->Unit(benchmark::kMillisecond)
     ->UseRealTime();
 BENCHMARK_TEMPLATE(BM_Sort32BitInts, sorting::Type::kGnuMultiwayMergesort)
-    ->DenseRange(kDataSizeBegin, kDataSizeEnd, kDataSizeStep)
     ->Unit(benchmark::kMillisecond)
     ->UseRealTime();
 
-BENCHMARK_MAIN();
+// Run benchmarks.
+int main(int argc, char** argv) {
+  sorting_init::ParseCommandLineFlags(&argc, argv);
+  benchmark::Initialize(&argc, argv);
+  if (benchmark::ReportUnrecognizedArguments(argc, argv)) return 1;
+  benchmark::RunSpecifiedBenchmarks();
+  benchmark::Shutdown();
+  return 0;
+}
---
+++ examples/sorting/test/sorting_init.h
@@ -0,0 +1,37 @@
+// Copyright 2022 The Modcncy Authors. All rights reserved.
+// Use of this source code is governed by the license found in the LICENSE file.
+// -----------------------------------------------------------------------------
+//
+// Init test and benchmark configurations for the sorting examples.
+//
+// -----------------------------------------------------------------------------
+
+#ifndef EXAMPLES_SORTING_TEST_SORTING_INIT_H_
+#define EXAMPLES_SORTING_TEST_SORTING_INIT_H_
+
+#include <modcncy/flags.h>
+
+namespace sorting_init {
+
+// Declare command line flags to be used.
+MODCNCY_DECLARE_int32(data_shift);
+MODCNCY_DECLARE_int32(segment_size);
+MODCNCY_DECLARE_int32(num_threads);
+
+// =============================================================================
+// Parses the declared command line flags.
+void ParseCommandLineFlags(int* argc, char** argv) {
+  for (int i = 1; i < *argc; ++i) {
+    if (modcncy::ParseInt32Flag(argv[i], "data_shift", &FLAGS_data_shift) ||
+        modcncy::ParseInt32Flag(argv[i], "segment_size", &FLAGS_segment_size) ||
+        modcncy::ParseInt32Flag(argv[i], "num_threads", &FLAGS_num_threads)) {
+      for (int j = i; j != *argc - 1; ++j) argv[j] = argv[j + 1];
+      --(*argc);
+      --i;
+    }
+  }
+}
+
+}  // namespace sorting_init
+
+#endif  // EXAMPLES_SORTING_TEST_SORTING_INIT_H_
